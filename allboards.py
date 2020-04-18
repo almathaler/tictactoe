bdad0651d8b1fb05b9677be2824b1f3fedcc884c
almathaler <athaler00@stuy.edu>
2020-04-18 01:41:01 -0400

    UNTESTED

--- allboards.py
+++ allboards.py
@@ -32,8 +32,45 @@ class BoardNode:
 #I took out the parent param cuz I found it unecessary -- the num of _ should work the same
 def CreateAllBoards(layout):
     global wins, AllBoards
-    # recursive function to manufacture all BoardNode nodes and place them into the AllBoards dictionary
-    return 0
+    #draft:
+    if layout not in AllBoards.keys(): #to avoid redundant trees
+        node = BoardNode(layout)
+        #check if it's a winner, cuz if so obv don't add children
+        winner = False
+        i = 0
+        while !winner and i < 8:
+            if layout[wins[i][0]] != '_' and layout[wins[i][0]] == layout[wins[i][1]] and layout[wins[i][1]] == layout[wins[i][2]]:
+                winner = True
+            else:
+                i+=1
+            #now that we know which position is winning, see which character it is
+        if winner:
+            node.endState = layout[wins[i][0]]
+        elif '_' not in layout: #means it's full. This and above are the recursive base cases
+            node.endState = 'd'
+        else:
+            #since it's not a winner, and not full, endState is None and you can add children.
+            turn = layout.count('_') #if it's odd, then it's x turn and if it's even then it's o
+            if turn % 2 == 0:
+                turn = 'o'
+            else:
+                turn = 'x'
+            #now find all the places you can add x or o
+            i = 0
+            spots = []
+            while i < 9:
+                if layout[i] == '_':
+                    spots.append(i)
+                i+=1
+            #now fill out the children, these will spawn their own CreateAllBoards calls 
+            children = []
+            for spot in spots:
+                child = layout[:spot] + turn + layout[spot+1:]
+                children.append(CreateAllBoards(child)) #remember this is valid cuz cab returns the layout
+            node.children = children
+        AllBoards[layout] = node
+    #returns layout so that it's easy to build AllBoards
+    return layout
 
 '''
 * print out # of boards in AllBoards
